247c1737760766fc0791097b58471a1c361f26e6
panhongbo <hongbo.pan@secneo.com>
2017-06-09 10:33:00 +0800

    add Repository

--- demo-dao/src/main/java/com/panhb/demo/dao/UserRepository.java
+++ demo-dao/src/main/java/com/panhb/demo/dao/UserRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.panhb.demo.entity.User;
+import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(path = "user")
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
 	
 	public User findByUserName(@Param(value = "userName") String userName);
