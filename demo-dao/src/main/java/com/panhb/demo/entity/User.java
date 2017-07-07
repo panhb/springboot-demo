013c7c2c7aadba7766c4791127e235957b406264
panhongbo <hongbo.pan@secneo.com>
2017-07-07 17:09:45 +0800

    pom打包修改

--- demo-dao/src/main/java/com/panhb/demo/entity/User.java
+++ demo-dao/src/main/java/com/panhb/demo/entity/User.java
@@ -17,8 +17,11 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
 import com.panhb.demo.model.base.BaseModel;
+import lombok.Data;
+
 @Entity
 @Table(name = "t_user")
+@Data
 public class User extends BaseModel{
 
 	private static final long serialVersionUID = 775638143722924325L;
@@ -37,56 +40,56 @@ public class User extends BaseModel{
 			inverseJoinColumns = {@JoinColumn(name = "roleId", referencedColumnName ="id")})
 	private List<Role> roles;
 	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	public String getPassWord() {
-		return passWord;
-	}
-	
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
-	}
-	
-	public Date getCreateDate() {
-		return createDate;
-	}
-	
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
-	
-	public List<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
-	
-	@Override
-	public String toString() {
-		return MoreObjects.toStringHelper(this)
-				.add("id", id)
-				.add("userName", userName)
-				.add("passWord", passWord)
-				.add("createDate", createDate)
-				.add("roles", roles)
-				.toString();
-	}
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getUserName() {
+//		return userName;
+//	}
+//
+//	public void setUserName(String userName) {
+//		this.userName = userName;
+//	}
+//
+//	public String getPassWord() {
+//		return passWord;
+//	}
+//
+//	public void setPassWord(String passWord) {
+//		this.passWord = passWord;
+//	}
+//
+//	public Date getCreateDate() {
+//		return createDate;
+//	}
+//
+//	public void setCreateDate(Date createDate) {
+//		this.createDate = createDate;
+//	}
+//
+//	public List<Role> getRoles() {
+//		return roles;
+//	}
+//
+//	public void setRoles(List<Role> roles) {
+//		this.roles = roles;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return MoreObjects.toStringHelper(this)
+//				.add("id", id)
+//				.add("userName", userName)
+//				.add("passWord", passWord)
+//				.add("createDate", createDate)
+//				.add("roles", roles)
+//				.toString();
+//	}
 	
 	
 
